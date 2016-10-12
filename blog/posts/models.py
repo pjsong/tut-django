7943c1cffa13fe38233f784f4ce5e9fa0aab8862
jeson_peng <jeson.peng@gmail.com>
2016-10-12 18:18:40 +0800

    finish advancing-branch

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,12 +1,19 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
 from django.utils import timezone
-
+from django.utils.safestring import mark_safe
 from django.utils.text import slugify
+
+
+from markdown_deux import markdown
+from comments.models import Comment
+
+from .utils import get_read_time
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
 
@@ -24,13 +31,10 @@ def upload_location(instance, filename):
     #filebase, extension = filename.split(".")
     #return "%s/%s.%s" %(instance.id, instance.id, extension)
     PostModel = instance.__class__
-    if(PostModel.objects.exists()):
-        new_id = PostModel.objects.order_by("id").last().id + 1
-    else:
-        new_id = 1
+    new_id = PostModel.objects.order_by("id").last().id + 1
     """
     instance.__class__ gets the model Post. We must use this method because the model is defined below.
-    Then create a queryset ordered by the "id"s of each object,
+    Then create a queryset ordered by the "id"s of each object, 
     Then we get the last object in the queryset with `.last()`
     Which will give us the most recently created Model instance
     We add 1 to it, so we get what should be the same id as the the post we are creating.
@@ -41,16 +45,17 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to=upload_location,
-            null=True,
-            blank=True,
-            width_field="width_field",
+    image = models.ImageField(upload_to=upload_location, 
+            null=True, 
+            blank=True, 
+            width_field="width_field", 
             height_field="height_field")
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
     content = models.TextField()
     draft = models.BooleanField(default=False)
     publish = models.DateField(auto_now=False, auto_now_add=False)
+    read_time =  models.IntegerField(default=0) # models.TimeField(null=True, blank=True) #assume minutes
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
@@ -68,6 +73,22 @@ class Post(models.Model):
     class Meta:
         ordering = ["-timestamp", "-updated"]
 
+    def get_markdown(self):
+        content = self.content
+        markdown_text = markdown(content)
+        return mark_safe(markdown_text)
+
+    @property
+    def comments(self):
+        instance = self
+        qs = Comment.objects.filter_by_instance(instance)
+        return qs
+
+    @property
+    def get_content_type(self):
+        instance = self
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        return content_type
 
 
 def create_slug(instance, new_slug=None):
@@ -82,11 +103,24 @@ def create_slug(instance, new_slug=None):
     return slug
 
 
-#docs.djangoproject.com/en/1.9/ref/signals/#presave
 def pre_save_post_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = create_slug(instance)
 
+    if instance.content:
+        html_string = instance.get_markdown()
+        read_time_var = get_read_time(html_string)
+        instance.read_time = read_time_var
 
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
+
+
+
+
+
+
+
+
+
+
--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -7,15 +7,13 @@ from .views import (
 	post_detail,
 	post_update,
 	post_delete,
-	post_index,
 	)
 
 urlpatterns = [
-	url(r'^$', post_index),
-	url(r'^list/$', post_list, name='list'),
-	url(r'^create/$', post_create),
-	url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
-	url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
-	url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
-	# url(r'^posts/$', "<appname>.views.<function_name>"),
+	url(r'^$', post_list, name='list'),
+    url(r'^create/$', post_create),
+    url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
+    #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
